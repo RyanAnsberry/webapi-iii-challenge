221ed5019aa8046a768fa81321cc31ca9a5fc992
Ryan Ansberry <ansman92@gmail.com>
2019-08-13 18:45:59 -0700

    Custom Middlewares

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
+const UserRouter = require('./users/userRouter.js');
+// const PostRouter = require('./posts/postRouter.js');
+
 const server = express();
 
 server.use(express.json());
 server.use(logger);
+server.use('/api/user', UserRouter);
+// server.use('/api/post', PostRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -12,10 +17,7 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-  console.log(
-    `[${new Date().toISOString()}] ${req.method} 
-      to ${req.url} from ${req.get('origin')}`
-  );
+  console.log( `[${new Date().toISOString()}] ${req.method} to ${req.url}` );
   next();
 };
 
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,42 +1,74 @@
 const express = 'express';
 
+const db = require('./userDb');
+
 const router = express.Router();
 
 router.post('/', (req, res) => {
 
+    db.insert()
 });
 
-router.post('/:id/posts', (req, res) => {
-
-});
-
-router.get('/', (req, res) => {
+router.post('/:id/posts', validateUserId, (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/', validateUserId, (req, res) => {
 
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
 
 });
 
-router.delete('/:id', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
 
 });
 
-router.put('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
+
+});
+
+router.put('/:id', validateUserId, (req, res) => {
 
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
+    const { id } = req.params;
 
+    db.getById(id)
+    .then(user => {
+        if (user) {
+            next();
+        } else {
+            res.status(400).json({ message: 'Invalid user id' });
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error processing request'
+        });
+    });
 };
 
 function validateUser(req, res, next) {
+    const newUser = req.body;
+    console.log('new user', newUser);
+    try {
+        if (!newUser) {
+            res.status(400).json({ message: 'Missing user data' });
+        } else if(!newUser.name) {
+            res.status(400).json({ message: "missing required name field" })
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error processing request'
+        });
+    }
 
 };
 
