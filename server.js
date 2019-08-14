8a2774da6e326bbaa3e9e69956849a756d1e819d
Ryan Ansberry <ansman92@gmail.com>
2019-08-14 13:42:23 -0700

    Completed api calls and middleware

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = express();
 
 server.use(express.json());
 server.use(logger);
-server.use('/api/user', UserRouter);
+server.use('/api/users', UserRouter);
 // server.use('/api/post', PostRouter);
 
 server.get('/', (req, res) => {
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,36 +1,87 @@
-const express = 'express';
+const express = require('express');
 
 const db = require('./userDb');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-
-    db.insert()
+router.post('/',validateUser ,(req, res) => {
+    const  { name }  = req.body;
+    db.insert({ name })
+    .then(response => {
+        res.json(response);
+    })
+    .catch(err => {
+        res.status(500).json({ message: `Error creating user, ${err.message}` });
+    });
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
-
+router.post('/:id/posts', validatePost, (req, res) => {
+    const { userId, text } = req.body;
+    posts
+      .insert({ userId, text })
+      .then(post => {
+        res.json(post);
+      })
+      .catch(err => {
+        res.status(500).json({ message: `Database error, ${err.message}` });
+      });
 });
 
-router.get('/', validateUserId, (req, res) => {
-
+router.get('/', (req, res) => {
+    db.get()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+        res.status(500).json({ message: `Database error, ${err.message}` });
+    });
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-
-});
-
-router.get('/:id/posts', validateUserId, (req, res) => {
-
+    const { id } = req.params;
+    db.getById(id)
+      .then(user => {
+        if (user === 0) {
+            res.status(404).json({ message: `No user by that Id in the DB, ${err.message}` });
+        }
+        res.json(user);
+      })
+      .catch(err => {
+        res.status(500).json({ message: `Database error, ${err.message}` });
+      });
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
-
+    const { id } = req.params;
+    db.remove(id)
+    .then(userRemoved => {
+      if (userRemoved === 0) {
+        res.status(404).json({ message: `No user by that Id, ${err.message}` });
+      } else {
+        res.json({ success: 'User Removed' });
+      }
+    })
+    .catch(err => {
+        res.status(500).json({ message: `Database error, ${err.message}` });
+    });
 });
 
 router.put('/:id', validateUserId, (req, res) => {
-
+    const { id } = req.params;
+    const { name } = req.body;
+    db.update( {user_id: id},  {name})
+      .then(response => {
+        if (response === 0) {
+            res.status(404).json({ message: `No user by that Id, ${err.message}` });
+        } else {
+          db.find(id).then(user => {
+            res.json(user);
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: `Database error, ${err.message}` });
+      });
 });
 
 //custom middleware
@@ -55,13 +106,12 @@ function validateUserId(req, res, next) {
 };
 
 function validateUser(req, res, next) {
-    const newUser = req.body;
-    console.log('new user', newUser);
+    const { name } = req.body;
     try {
-        if (!newUser) {
-            res.status(400).json({ message: 'Missing user data' });
-        } else if(!newUser.name) {
+        if (!name) {
             res.status(400).json({ message: "missing required name field" })
+        } else {
+            next();
         }
     } catch (error) {
         console.log(error);
@@ -69,11 +119,26 @@ function validateUser(req, res, next) {
             message: 'Error processing request'
         });
     }
-
 };
 
-function validatePost(req, res, next) {
+function validateUser(req, res, next) {
+    const { name } = req.body;
+    if (!name) {
+        res.status(400).json({ message: "missing required name field" });
+    } else {
+      next();
+    }
+  };
 
+function validatePost(req, res, next) {
+    const { postBody }= req.body;
+    if(!postBody) {
+        res.status(400).json({ message: "missing post data" });
+    } else if (!postBody.name) {
+        res.status(400).json({ message: "missing required text field" });
+    } else {
+        next();
+    }
 };
 
 module.exports = router;
